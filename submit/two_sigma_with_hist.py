ecaf97e91098ddf74e0da87b5e93a59946c5d16d
georgy.samoylov@rockscie.com <georgy.samoylov@rockscie.com>
2019-01-03 00:19:14 +0300

    Market features

--- submit/two_sigma_with_hist.py
+++ submit/two_sigma_with_hist.py
@@ -17,13 +17,23 @@ except Exception as error:
     from all import env_for_test as twosigmanews
 
 
+def FSmaCrossing(close: pd.Series,
+                 window) -> pd.DataFrame:
+    f_sma_1 = close.rolling(window=window, min_periods=window).mean()
+    f_sma_2 = close.rolling(window=2 * window,
+                            min_periods=2 * window).mean()
+
+    result = (f_sma_1 - f_sma_2) / f_sma_2
+    return result.to_frame()
+
+
 class Data:
     """
     save all data and create feature
     """
 
     news_cols_agg = {
-        'urgency':            ['min', 'count'],
+        'urgency':            ['min', 'max', 'count'],
         'takeSequence':       ['max'],
         'bodySize':           ['min', 'max', 'mean', 'std'],
         'wordCount':          ['min', 'max', 'mean', 'std'],
@@ -65,8 +75,16 @@ class Data:
         define start data and base columns
         """
 
-        self._ALLMarket = market  # type: pd.DataFrame
-        self._ALLNews = news  # type: pd.DataFrame
+        market.loc[:, self.TIME ] = pd.to_datetime(
+                market[self.TIME]
+                ).dt.strftime("%Y%m%d").astype(int)
+
+        self._ALLMarket = market[market[self.TIME]> 20101010] # type: pd.DataFrame
+
+        news.loc[:, self.TIME] = pd.to_datetime(
+                news[self.TIME]
+                ).dt.strftime("%Y%m%d").astype(int)
+        self._ALLNews = news[news[self.TIME]> 20101010]  # type: pd.DataFrame
 
         # 2sec
         self.not_univers_ticker, self.univers_ticker = [[
@@ -80,32 +98,58 @@ class Data:
         """
         save new data in atr. !! there are no check be carefully
         """
+        market.loc[:, self.TIME] = pd.to_datetime(
+                market[self.TIME]
+                ).dt.strftime("%Y%m%d").astype(int)
         self._ALLMarket = self._ALLMarket.append(market, ignore_index=True)
+        news.loc[:, self.TIME] = pd.to_datetime(
+                news[self.TIME]
+                ).dt.strftime("%Y%m%d").astype(int)
         self._ALLNews = self._ALLNews.append(news, ignore_index=True)
 
     def get_features(self, ticker):
         market = self._ALLMarket[
             self._ALLMarket[self.ASSET] == ticker]  # 0.2sec
-        market.loc[:, self.TIME + 'tmp'] = pd.to_datetime(
-                market[self.TIME]
-                ).dt.strftime("%Y%m%d").astype(int)
 
-        news = self._ALLNews[self._ALLNews['assetCodes'].str.contains(
-                "'" + ticker + "'",
-                regex=False)]
-        news.loc[:, self.ASSET] = ticker
-        news.loc[:, self.TIME + 'tmp'] = pd.to_datetime(
-                news[self.TIME]
-                ).dt.strftime("%Y%m%d").astype(int)
-        news = news.groupby(
-                [self.TIME + 'tmp', self.ASSET]
-                ).agg(self.news_cols_agg)
+
+        try:
+            news = self._ALLNews[self._ALLNews['assetCodes'].str.contains(
+                    "'" + ticker + "'",
+                    regex=False)]
+
+            news = news.groupby(
+                    [self.TIME]
+                    ).agg(self.news_cols_agg)
+
+            market.join(news, on=[self.TIME])
+        except Exception as error:
+            print("there is no news for " , ticker)
+        #todo spped up
+        result = pd.DataFrame()
+        result.loc[:, 'bartrend'] = market['close'] / market['open']
+        result.loc[:, 'crosSMA10Close'] = FSmaCrossing(market['close'], 10)
+        result.loc[:, 'crosSMA50Close'] = FSmaCrossing(market['close'], 50)
+        result.loc[:, 'crosSMA50open'] = FSmaCrossing(market['open'], 50)
+        result.loc[:, 'gap'] = market['close'].shift(1) / market['open']
+        result.loc[:, 'volume'] = FSmaCrossing(market['volume'], 50)
+        result.loc[:, ('urgency', 'min')] = market[('urgency', 'min')].fillna(0)
+        result.loc[:, ('urgency', 'max')] = market[('urgency', 'max')].fillna(0)
+        result.loc[:, ('urgency', 'std')] = market[('urgency', 'std')].fillna(0)
+        result.loc[:, ('urgency sma', 'max')] = market[
+            ('urgency', 'max')].ffill().mean(20)
+        result.loc[:, ('urgency', 'min')] = market[
+            ('urgency', 'min sma')].ffill().mean(20)
+
+        result.loc[:, 'companyCount'] = market['companyCount', 'max'].fillna(
+            0)
+
+        #todo add sentiment feature
 
         # todo aggregate look on kernal https://www.kaggle.com/bguberfain/a-simple-model-using-the-market-and-news-data
 
         # todo create features
 
-        return market.join(news, on=[self.TIME + 'tmp', self.ASSET])
+        return market
 
 
 if __name__ == '__main__':
@@ -113,9 +157,18 @@ if __name__ == '__main__':
 
     market_df, news_df = ENV.get_training_data()
     data_obj = Data(market_df, news_df)
+    print(data_obj.univers_ticker)
+    market = data_obj._ALLMarket
+    result = []
 
-    for ticker in data_obj.univers_ticker[:1]:
-        #todo  join diff tickers
+    for ticker , df in  market.groupby(data_obj.ASSET):
+        ret = df.loc[250:,'returnsOpenNextMktres10'] # type: pd.Series
+        profit = ret.mean()
+        sharpe = profit/ ret.std()
+        result.append([ticker,sharpe])
 
-        ticker_date = data_obj.get_features(ticker)
-        ticker_date.to_csv(ticker + "feature.csv")
+
+    # for ticker in data_obj.univers_ticker:
+    #     # todo  join diff tickers
+    #     if ticker == 'ARRO.O':
+    #         data_obj.get_features(ticker).to_csv(ticker+'.csv')
